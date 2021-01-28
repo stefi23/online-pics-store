178ef3d7461013da1f20f2344e7c2217e646d1bb
stefi23 <stefania.rosca2309@gmail.com>
2021-01-28 15:32:19 +0200

    added hover effects for images

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Image({className, img}){
+    const [isHoovered, setIsHoovered] = useState(false)
+
+    const handleMouseEnter = () =>{
+        setIsHoovered(true)
+    }
+
+    const handleMouseOut = () =>{
+        setIsHoovered(false)
+    }
 
     return(
         <div className={`${className} image-container`}  >
-        <img  src={img} className='image-grid'/>
+        <img    onMouseEnter={() => setIsHoovered(true)}
+                onMouseLeave={() => setIsHoovered(false)}
+                src={img} className='image-grid'/>
+                {isHoovered ? (
+                    <>
+                <i className="ri-heart-line favorite"></i>
+                <i className="ri-add-circle-line cart"></i>
+                </>
+                ) : null }
+
          </div>
     )
 }
